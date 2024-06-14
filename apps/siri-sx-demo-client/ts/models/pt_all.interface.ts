6fefe6443e4c24fbe0fefc97e47477cd7d670b00
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-06-14 14:00:42 +0200

    Adds affects models

--- apps/siri-sx-demo-client/ts/models/pt_all.interface.ts
+++ apps/siri-sx-demo-client/ts/models/pt_all.interface.ts
@@ -2,6 +2,52 @@ export type TextualContentSizeEnum = 'small' | 'medium' | 'large'
 export type LangEnum = 'de' | 'fr' | 'it' | 'en'
 export type ScopeType = 'line' | 'stopPlace' | 'vehicleJourney'
 
+interface NetworkOperator {
+    operatorRef: string
+}
+
+export interface LineNetwork {
+    operator: NetworkOperator
+    lineRef: string
+    publishedLineName: string
+}
+
+export interface AffectedLineNetworkWithStops {
+    lineNetwork: LineNetwork
+    directionRef: string
+    stopPlaces: StopPlace[]
+}
+
+export interface StopPlace {
+    stopPlaceRef: string
+    placeName: string
+}
+
+export interface AffectedStopPlace {
+    stopPlaceRef: string
+    placeName: string | null
+}
+
+export interface FramedVehicleJourneyRef {
+    dataFrameRef: string
+    datedVehicleJourneyRef: string
+}
+
+export interface AffectedVehicleJourney {
+    framedVehicleJourneyRef: FramedVehicleJourneyRef
+    operator: NetworkOperator
+    origin: AffectedStopPlace | null
+    destination: AffectedStopPlace | null
+    callStopsRef: string[]
+    lineRef: string | null
+    publishedLineName: string | null
+}
+
+export interface PublishingActionAffect {
+    type: 'stop' | 'entire-line' | 'partial-line' | 'vehicle-journey'
+    affect:  StopPlace | LineNetwork | AffectedLineNetworkWithStops | AffectedVehicleJourney
+}
+
 export interface PublishingAction {
     scopeType: ScopeType
     passengerInformation: PassengerInformationAction
