c3fc45e4faad6fc996c9ccd63217bdcb23feccab
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-08-22 18:03:04 +0200

    Adds HTTP client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideHttpClient(), provideRouter(routes)]
 };
